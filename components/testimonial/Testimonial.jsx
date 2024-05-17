68ee535525f325d33b8a136134d0ffd2ca88b7e4
Roshan <roshan@racloop.com>
2024-05-17 15:51:12 +0530

    update website

--- components/testimonial/Testimonial.jsx
+++ components/testimonial/Testimonial.jsx
@@ -5,12 +5,13 @@ import Slider from "react-slick";
 export default function SimpleSlider() {
   var settings = {
     dots: true,
-    arrow: false,
+    arrows: false, // Ensure this is "arrows" not "arrow"
     infinite: true,
     speed: 900,
     slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay: false,
+    slidesToScroll: 1, // Set this to 1 for smoother continuous slide
+    autoplay: true,
+    autoplaySpeed: 3000, // Speed in milliseconds for autoplay
     margin: 30,
     responsive: [
       {
@@ -23,66 +24,59 @@ export default function SimpleSlider() {
   };
 
   const TestimonilContent = [
-   {
+    {
       imageName: "pankaj.jpeg",
-      desc: `  I design and develop services for customers of all sizes,
+      desc: `I design and develop services for customers of all sizes,
       specializing in creating stylish, modern websites, web services
       and online stores.`,
       reviewerName: "Pankaj",
       rating: "5.0",
     },
-     {
+    {
       imageName: "mona.jpeg",
-      desc: `  I design and develop services for customers of all sizes,
+      desc: `I design and develop services for customers of all sizes,
       specializing in creating stylish, modern websites, web services
       and online stores.`,
       reviewerName: "Mona",
       rating: "5.0",
     },
-   
     {
       imageName: "rajan.jpeg",
-      desc: `  I design and develop services for customers of all sizes,
+      desc: `I design and develop services for customers of all sizes,
       specializing in creating stylish, modern websites, web services
       and online stores.`,
       reviewerName: "Rajan",
       rating: "4.8",
     },
-    
-   
   ];
 
   return (
-    <>
-      <Slider {...settings}>
-        {TestimonilContent.map((val, i) => (
-        
-          <div className="testmonial-box media" key={i}>
-              {console.log('val',val)}
-            <div className="avatar">
-              <Image
-                width={638}
-                height={690}
-                style={{ height: "fit-content" }}
-                src={`/img/${val.imageName}`}
-                alt="review comments"
-              />
-            </div>
-            {/* End avatar */}
-            <div className="testmonial-text media-body">
-              <p>{val.desc}</p>
-              <div className="testmonial-info">
-                <h6>{val.reviewerName}</h6>
-                <div className="rating-value">
-                  <i className="ti-star"></i>
-                  <span>{val.rating}</span>
-                </div>
+    <Slider {...settings}>
+      {TestimonilContent.map((val, i) => (
+        <div className="testmonial-box media" key={i}>
+          <div className="avatar">
+            <Image
+              width={638}
+              height={690}
+              style={{ height: "fit-content" }}
+              src={`/img/${val.imageName}`}
+              alt="review comments"
+            />
+          </div>
+          {/* End avatar */}
+          <div className="testmonial-text media-body">
+            <p>{val.desc}</p>
+            <div className="testmonial-info">
+              <h6>{val.reviewerName}</h6>
+              <div className="rating-value">
+                {/* <i className="ti-star"></i> */}
+                {/* <span>{val.rating}</span> */}
               </div>
             </div>
-            {/* End testimonial-text */}
           </div>
-        ))}
-      </Slider>
-    </>
+          {/* End testimonial-text */}
+        </div>
+      ))}
+    </Slider>
   );
 }
